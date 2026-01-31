a7d7603fa10bb61708944310b31b38a85acd789f
SAITO Atsushi <maldikulo@gmail.com>
2026-01-31 12:31:31 +0900

    Now it can obtain command-line arguments without using the standard library.

--- src/command.rs
+++ src/command.rs
@@ -1,4 +1,9 @@
 use std::default::Default;
+use std::mem::MaybeUninit;
+use windows_sys::Win32::Foundation::{GetLastError, WIN32_ERROR};
+use windows_sys::Win32::System::Environment::GetCommandLineW;
+use windows_sys::Win32::UI::Shell::CommandLineToArgvW;
+use windows_sys::w;
 
 pub struct Cmd {
     pub horizontal: bool,
@@ -10,7 +15,9 @@ pub enum Error {
     UnknownOption,
     Help,
     ParseErrorSecond,
-    Version(String),
+    Version,
+    FromUtf16Error,
+    SystemErrorCode(u32),
 }
 
 impl std::fmt::Display for Error {
@@ -18,9 +25,28 @@ impl std::fmt::Display for Error {
         match self {
             Error::UnknownOption => write!(f, "解釈できないオプションです。"),
             Error::Help => write!(f, include_str!("usage.txt")),
-            Error::Version(name) => write!(f, "{} v{}", name, env!("CARGO_PKG_VERSION")),
+            Error::Version => write!(
+                f,
+                "{} v{}",
+                env!("CARGO_BIN_NAME"),
+                env!("CARGO_PKG_VERSION")
+            ),
             Error::ParseErrorSecond => write!(f, "秒の指定を解釈できませんでした。"),
+            Error::SystemErrorCode(n) => write!(f, "ウィンドウズのシステムエラーです。 ({})", n),
+            Error::FromUtf16Error => write!(f, "コマンドラインの文字コードの解釈に失敗しました。"),
         }
+    }
+}
+
+impl From<WIN32_ERROR> for Error {
+    fn from(value: WIN32_ERROR) -> Self {
+        Self::SystemErrorCode(value)
+    }
+}
+
+impl From<std::string::FromUtf16Error> for Error {
+    fn from(_: std::string::FromUtf16Error) -> Self {
+        Self::FromUtf16Error
     }
 }
 
@@ -33,46 +59,117 @@ enum State {
     Ncodes,
 }
 
+struct Argv<'a> {
+    argv: &'a [*mut u16],
+}
+
+struct Arg {
+    arg: *mut u16,
+}
+
+impl Arg {
+    fn as_slice(&self) -> &[u16] {
+        let mut i = 0;
+        while unsafe { *self.arg.add(i) } != 0 {
+            i += 1;
+        }
+        unsafe { std::slice::from_raw_parts(self.arg, i) }
+    }
+}
+
+struct Args<'a> {
+    argv: &'a [*mut u16],
+    count: usize,
+}
+
+impl Iterator for Args<'_> {
+    type Item = Arg;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count >= self.argv.len() {
+            None
+        } else {
+            let count = self.count;
+            self.count += 1;
+            Some(Arg {
+                arg: self.argv[count],
+            })
+        }
+    }
+}
+
+impl<'a> Argv<'a> {
+    fn new() -> Result<Self, WIN32_ERROR> {
+        unsafe {
+            let mut argc: MaybeUninit<i32> = MaybeUninit::uninit();
+            let arg_list = CommandLineToArgvW(GetCommandLineW(), argc.as_mut_ptr());
+            if arg_list.is_null() {
+                return Err(GetLastError());
+            } else {
+                Ok(Self {
+                    argv: std::slice::from_raw_parts_mut(arg_list, argc.assume_init() as _),
+                })
+            }
+        }
+    }
+
+    fn iter(&'a self) -> Args<'a> {
+        Args {
+            argv: self.argv,
+            count: 0,
+        }
+    }
+}
+
+impl PartialEq<*const u16> for Arg {
+    fn eq(&self, other: &*const u16) -> bool {
+        let mut p = self.arg;
+        let mut q = *other;
+        while unsafe { *p } != 0 && unsafe { *q } != 0 && unsafe { *p } == unsafe { *q } {
+            unsafe {
+                p = p.add(1);
+                q = q.add(1);
+            }
+        }
+        unsafe { *p == *q }
+    }
+}
+
 impl Cmd {
     pub fn parse() -> Result<Self, Error> {
         let mut horizontal = false;
-        let mut command_name = None;
         let mut state = Default::default();
         let mut wait = 1.0;
         let mut ncodes: Vec<String> = vec![];
-        for i in std::env::args() {
+        for i in Argv::new()?.iter() {
             state = match state {
-                State::Start => {
-                    command_name = Some(i);
-                    State::Options
-                }
-                State::Options => match &i[..] {
-                    "--horizontal" => {
+                State::Start => State::Options,
+                State::Options => {
+                    if i == w!("--horizontal") {
                         horizontal = true;
                         State::Options
-                    }
-                    "--wait" | "-w" => State::Wait,
-                    "--help" | "-h" => {
+                    } else if i == w!("--wait") || i == w!("-w") {
+                        State::Wait
+                    } else if i == w!("--help") || i == w!("-h") {
                         return Err(Error::Help);
-                    }
-                    "--version" | "-V" => {
-                        return Err(Error::Version(command_name.unwrap()));
-                    }
-                    "--" => State::Ncodes,
-                    s if s.starts_with('-') => {
+                    } else if i == w!("--version") || i == w!("-V") {
+                        return Err(Error::Version);
+                    } else if i == w!("--") {
+                        State::Ncodes
+                    } else if i.as_slice().starts_with(&['-' as u16]) {
                         return Err(Error::UnknownOption);
-                    }
-                    _ => {
-                        ncodes.push(i);
+                    } else {
+                        ncodes.push(String::from_utf16(i.as_slice())?);
                         State::Ncodes
                     }
-                },
+                }
                 State::Wait => {
-                    wait = i.parse::<f64>().or(Err(Error::ParseErrorSecond))?;
+                    wait = String::from_utf16(i.as_slice())?
+                        .parse::<f64>()
+                        .or(Err(Error::ParseErrorSecond))?;
                     State::Options
                 }
                 State::Ncodes => {
-                    ncodes.push(i);
+                    ncodes.push(String::from_utf16(i.as_slice())?);
                     State::Ncodes
                 }
             }
